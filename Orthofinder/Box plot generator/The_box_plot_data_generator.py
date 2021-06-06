f00537372068bead47576cb52112961d879b4c82
Rijan dhakal <dhakalrijan825@gmail.com>
2021-06-06 18:44:10 -0500

    Yip

--- Orthofinder/Box plot generator/The_box_plot_data_generator.py
+++ Orthofinder/Box plot generator/The_box_plot_data_generator.py
@@ -10,10 +10,15 @@ def main():
         Species_list_lines = Species_list.readlines()
 
     for ortho_line in Filtered_list_lines:
-        for gene_name in Species_list_lines:
+        #print(ortho_line[:20])
+        for gene_name in Gene_list_lines:
+            gene_name = gene_name.rstrip('\n')
+            #print(gene_name)
             for species_identifier in Species_list_lines:
-                if species_identifier in ortho_line:
-                    current_line_count = ortho_line.count(species_identifier)
-                    print(gene_name,species_identifier,current_line_count)
+                species_identifier = species_identifier.rstrip('\n')
+                #print(species_identifier)
+                #if species_identifier in ortho_line:
+                current_line_count = ortho_line.count(species_identifier)
+                print(gene_name,species_identifier,current_line_count, sep="\t")
         
 main()
