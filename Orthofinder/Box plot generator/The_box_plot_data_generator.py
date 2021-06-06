f55917dec4aceee7a80264baca8a3ae6604f50c3
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-06-06 15:53:14 -0500

    just some minor update

--- Orthofinder/Box plot generator/The_box_plot_data_generator.py
+++ Orthofinder/Box plot generator/The_box_plot_data_generator.py
@@ -10,5 +10,10 @@ def main():
         Species_list_lines = Species_list.readlines()
 
     for ortho_line in Filtered_list_lines:
+        for gene_name in Species_list_lines:
+            for species_identifier in Species_list_lines:
+                if species_identifier in ortho_line:
+                    current_line_count = ortho_line.count(species_identifier)
+                    print(gene_name,species_identifier,current_line_count)
         
 main()
