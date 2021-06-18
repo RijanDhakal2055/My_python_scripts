03dec518d81764d463f679d42c3e538ea829485a
Rijan Dhakal <dhakalrijan825@gmail.com>
2021-06-17 20:21:18 -0500

    Just a test

--- Tree_visualization_runs/Replace_names/use_dict.py
+++ Tree_visualization_runs/Replace_names/use_dict.py
@@ -4,13 +4,14 @@ def main():
 	with open('species_dict.pkl','rb') as handle:
 		the_dict = pickle.load(handle)
 
-	with open('Base_asr.tre','rt') as the_base_file:
-		the_base_file_line = the_base_file.readlines()
+	#with open('Base_asr.tre','rt') as the_base_file:
+		#the_base_file_line = the_base_file.readlines()
 
-	for lines in the_base_file_line:
-		x = 'AagrBONN'
-		if x in lines:
-			print(lines)
+	#for lines in the_base_file_line:
+
+	for line in the_dict:
+		print(line)
+		
 
 
 main()
