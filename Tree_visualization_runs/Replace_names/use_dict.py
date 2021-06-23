27fa3528f69b08531474ee0478011b466cdd095e
Rijan dhakal <dhakalrijan825@gmail.com>
2021-06-23 16:58:12 -0500

    To merger

--- Tree_visualization_runs/Replace_names/use_dict.py
+++ Tree_visualization_runs/Replace_names/use_dict.py
@@ -1,17 +1,19 @@
+from os import replace
 import pickle
+import re
 
 def main():
+
+	real_lines = []
 	with open('species_dict.pkl','rb') as handle:
 		the_dict = pickle.load(handle)
 
-	#with open('Base_asr.tre','rt') as the_base_file:
-		#the_base_file_line = the_base_file.readlines()
+	with open('Base_asr.tre','rt') as the_base_file:
+		the_base_file_line = the_base_file.readlines()
 
-	#for lines in the_base_file_line:
-
-	for line in the_dict:
-		print(line)
-		
-
+	for the_line in the_base_file_line:
+		for the_key in the_dict:
+			x = the_line.replace(the_key,the_dict[the_key])
+			print(x)	
 
 main()
