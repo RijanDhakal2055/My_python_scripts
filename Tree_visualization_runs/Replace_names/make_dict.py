fb2815db6555e685b6abe72a80c74941e27eb33e
Rijan Dhakal <dhakalrijan825@gmail.com>
2021-06-25 14:41:06 -0500

    The ones with the tree runs for cafe5

--- Tree_visualization_runs/Replace_names/make_dict.py
+++ Tree_visualization_runs/Replace_names/make_dict.py
@@ -4,7 +4,7 @@ def main():
 
 	species_identifiers = {}
 
-	with open("species_id.txt",'rt') as species_id_pairs:
+	with open("proper_names.txt",'rt') as species_id_pairs:
 		specie_id_line = species_id_pairs.readlines()
 
 	for current_line in specie_id_line:
@@ -17,7 +17,7 @@ def main():
 
 	#the print statement is just me trying to make sure that the cose is working properly
 
-	dict_file = open("species_dict.pkl","wb")
+	dict_file = open("species_dict2.pkl","wb")
 	pickle.dump(species_identifiers,dict_file)
 	dict_file.close()
 
