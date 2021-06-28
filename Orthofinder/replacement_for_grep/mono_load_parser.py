e6f95672b2feb097a6e84f9c94ccbdb614b593ab
Rijan Dhakal <dhakalrijan825@gmail.com>
2021-06-28 15:13:40 -0500

    about done here

--- Orthofinder/replacement_for_grep/mono_load_parser.py
+++ Orthofinder/replacement_for_grep/mono_load_parser.py
@@ -1,5 +1,5 @@
 def main():
-    with open('spindle_fiber_list.txt','rt') as small_list:
+    with open('chromatin_list.txt','rt') as small_list:
         small_list_lines = small_list.readlines()
 
     for i in small_list_lines:
