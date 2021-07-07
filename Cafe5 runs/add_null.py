e069579510fccfc0cb8d8e381fe59f4ba63680d8
Rijan <dhakalrijan825@gmail.com>
2021-07-07 00:59:56 -0500

    commit

--- Cafe5 runs/add_null.py
+++
@@ -1,8 +0,0 @@
-def main():
-	with open("new_ortho.tsv","rt") as old_file:
-		old_file_lines = old_file.readlines()
-
-	for i in old_file_lines:
-		print("(null)","\t",i)
-
-main()
--- Orthofinder/replacement_for_grep/grep_replacer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PATTERN=$1
-FILE=$2
-while IFS= read -r line; do
-	#echo "'$line'"
-	grep "$line" $FILE
-done < $1
