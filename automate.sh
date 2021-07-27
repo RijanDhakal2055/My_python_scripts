03f3651599fc176b33b9465c497e1e0abd6ae483
Rijan dhakal <Dhakalrijan825@gmail.com>
2021-07-27 13:47:16 -0500

    this one works except for one tiny bug

--- automate.sh
+++ automate.sh
@@ -4,7 +4,7 @@ cd /home/opensourceferns/Desktop/Script_atoll/Muscle
 
 for file in Muscle_test/*.fa
 do
-    muscle -in file -out file.afa
+    muscle -in $file -out $file.afa
 done
 
 ##this calls muscle but fails at calling specific files
