ca6712f925fc614202f641da4228a13589e6d4f3
Rijandhakal2055 <dhakalrijan825@gmail.com>
2021-07-27 15:26:52 -0500

    this is the commit

--- automate.sh
+++ automate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd /home/opensourceferns/Desktop/Script_atoll/Muscle
+cd /home/rijandhakal/Desktop
 
 for file in Muscle_test/*.fa
-do
-    muscle -in $file -out $file.afa
+do  
+    out_name="$(basename $file .fa)"
+    ext=".afa"
+    out_file="$out_name$ext"
+    echo out_file
+    muscle -in $file -out $out_file
 done
-
-##this calls muscle but fails at calling specific files
